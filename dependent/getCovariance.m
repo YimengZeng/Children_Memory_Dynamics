60762a20e6b387b543f405b2ee6b097be2c40be4
YimengZeng <30626747+YimengZeng@users.noreply.github.com>
2025-11-24 14:15:09 -0500

    Delete dependent directory

--- dependent/getCovariance.m
+++
@@ -1,5 +0,0 @@
-function cov_state = getCovariance(net)
-nStates = length(net.params.Lm);
-for state=1:nStates
-      cov_state{state}= net.params.Lm{state}(:,2:end)*net.params.Lm{state}(:,2:end)' + diag(net.hparams.psii)^-1;
-end
